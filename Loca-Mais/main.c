a0bac62ecad4b0e8c4e0276f69db4d4a3a546465
Vitor Lany <vitorlany99@gmail.com>
2022-06-20 11:18:27 -0300

    Inicio

---
+++ Loca-Mais/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct construtorEndereco{
+    char rua[50];
+    int numero;
+    int cep;
+};
+typedef struct construtorEndereco tipoEndereco;
+
+struct construtorCliente{
+    int codigo;
+    char nome[50];
+    tipoEndereco endereco;
+};
+typedef struct construtorCliente tipoCliente;
+
+
+int main()
+{
+    tipoCliente cliente;
+
+    cliente.codigo = 15;
+    strcpy(cliente.nome,"Vitor");
+    strcpy(cliente.endereco.rua, "Nome da Rua");
+    cliente.endereco.numero = 123;
+    cliente.endereco.cep = 123456;
+
+    printf("Cliente: %i, %s, %s, %i, %i", cliente.codigo, cliente.nome, cliente.endereco.rua, cliente.endereco.numero, cliente.endereco.cep);
+
+    return 0;
+}
