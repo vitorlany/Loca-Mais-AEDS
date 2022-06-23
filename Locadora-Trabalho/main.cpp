062b969e234df436599dc4c66fd5e88fb399b1b2
Vitor Lany <vitorlany99@gmail.com>
2022-06-23 09:33:29 -0300

    Cliente pronto + Comeco veiculos

--- Locadora-Trabalho/main.cpp
+++ Locadora-Trabalho/main.cpp
@@ -2,27 +2,67 @@
 
 using namespace std;
 
+int proximoCodigoCliente = 0;
+int proximoCodigoVeiculo = 0;
+
+// Clientes
+
+class ClasseClienteEndereco {
+    public:
+        string rua;
+        int numero;
+        int cep;
+};
+
 class ClasseCliente {
     public:
         int codigo;
         string nome;
+        ClasseClienteEndereco endereco;
         int telefone;
-        int deletada = 0;
-        class endereco {
-            public:
-                string rua;
-                int numero;
-                int cep;
-        };
+
+
+        ClasseCliente(string _nome, string _rua, int _numero, int _cep, int _telefone);
+};
+
+ClasseCliente::ClasseCliente(string _nome, string _rua, int _numero, int _cep, int _telefone) {
+    codigo = proximoCodigoCliente++;
+    nome = _nome;
+    endereco.rua = _rua;
+    endereco.numero = _numero;
+    endereco.cep = _cep;
+    telefone = _telefone;
+}
+
+// Veiculos
+
+class ClasseVeiculo {
+    public:
+        int codigo;
+        string descricao;
+        string modelo;
+        string cor;
+        string placa;
+        int valor;
+        int ocupantes;
+        // 0 = Alugado / 1 = Disponivel / 2 = Manutencao
+        int status = 1;
 };
 
 int main()
 {
-    ClasseCliente Cliente;
 
-    Cliente.nome = "Raul Fernandes";
-    Cliente.codigo = 123;
 
-    cout << Cliente.codigo << endl << Cliente.nome << endl << Cliente.deletada;
     return 0;
+}
+
+void exemploCliente() {
+    ClasseCliente cliente("Vitor", "Rua pipi", 123, 1256, 9999);
+
+    cout << "Codigo: " << cliente.codigo << endl;
+    cout << "Nome: " << cliente.nome << endl;
+    cout << "Rua: " << cliente.endereco.rua << endl;
+    cout << "Numero: " << cliente.endereco.numero << endl;
+    cout << "Cep: " << cliente.endereco.cep << endl;
+    cout << "Telefone: " << cliente.telefone << endl;
 }
