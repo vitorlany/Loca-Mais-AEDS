549eec6d535340d0a4d3a9ca79148a5fdb3f6da7
Vitor Lany <vitorlany99@gmail.com>
2022-06-28 16:17:15 -0300

    Calcular Data

--- Projeto-Final/main.cpp
+++ Projeto-Final/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+
 #include "ClasseCliente.h"
 #include "ClasseVeiculo.h"
 #include "ClasseLocacao.h"
@@ -8,7 +10,28 @@ using namespace std;
 
 int main()
 {
-    teste();
+    int umDia = 24 * 60 * 60;
+    int dias = 5;
+
+    time_t dataRetirada;
+    time(&dataRetirada);
+    char retiradaStr[80];
+    tm * retirada = localtime(&dataRetirada);
+
+    strftime(retiradaStr, 80, "%d/%m/%Y", retirada);
+
+    cout << retiradaStr << endl;
+
+
+    time_t dataDevolucao;
+    time(&dataDevolucao);
+    char devolucaoStr[80];
+    time_t novosDias = dataDevolucao + (dias * umDia);
+    tm * devolucao = localtime(&novosDias);
+
+    strftime(devolucaoStr, 80, "%d/%m/%Y", devolucao);
+
+    cout << devolucaoStr << endl;
 
     return 0;
 }
